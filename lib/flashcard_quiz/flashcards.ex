be0460f172f83eefc5fad1555b6c19e24cb042fc
Fly Dev <dev@fly.io>
2025-07-16 19:52:37 +0000

    update flashcards context for packs

--- lib/flashcard_quiz/flashcards.ex
+++ lib/flashcard_quiz/flashcards.ex
@@ -7,18 +7,126 @@ defmodule FlashcardQuiz.Flashcards do
   alias FlashcardQuiz.Repo
 
   alias FlashcardQuiz.Flashcards.Flashcard
+  alias FlashcardQuiz.Flashcards.Pack
 
   @doc """
-  Returns the list of flashcards.
+  Returns the list of packs.
+
+  ## Examples
+
+      iex> list_packs()
+      [%Pack{}, ...]
+
+  """
+  def list_packs do
+    Repo.all(Pack)
+  end
+
+  @doc """
+  Gets a single pack.
+
+  Raises `Ecto.NoResultsError` if the Pack does not exist.
+
+  ## Examples
+
+      iex> get_pack!(123)
+      %Pack{}
+
+      iex> get_pack!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_pack!(id), do: Repo.get!(Pack, id)
+
+  @doc """
+  Creates a pack.
+
+  ## Examples
+
+      iex> create_pack(%{field: value})
+      {:ok, %Pack{}}
+
+      iex> create_pack(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_pack(attrs \\ %{}) do
+    %Pack{}
+    |> Pack.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a pack.
+
+  ## Examples
+
+      iex> update_pack(pack, %{field: new_value})
+      {:ok, %Pack{}}
+
+      iex> update_pack(pack, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_pack(%Pack{} = pack, attrs) do
+    pack
+    |> Pack.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a pack.
+
+  ## Examples
+
+      iex> delete_pack(pack)
+      {:ok, %Pack{}}
+
+      iex> delete_pack(pack)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_pack(%Pack{} = pack) do
+    Repo.delete(pack)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking pack changes.
+
+  ## Examples
+
+      iex> change_pack(pack)
+      %Ecto.Changeset
+
+  """
+  def change_pack(%Pack{} = pack, attrs \\ %{}) do
+    Pack.changeset(pack, attrs)
+  end
+
+  @doc """
+  Returns the list of flashcards, optionally filtered by pack.
 
   ## Examples
 
       iex> list_flashcards()
       [%Flashcard{}, ...]
 
+      iex> list_flashcards(pack_id: 1)
+      [%Flashcard{}, ...]
+
   """
-  def list_flashcards do
-    Repo.all(from f in Flashcard, order_by: [asc: f.inserted_at])
+  def list_flashcards(opts \\ []) do
+    query = from(f in Flashcard, preload: [:pack])
+
+    case Keyword.get(opts, :pack_id) do
+      nil ->
+        Repo.all(query)
+
+      pack_id ->
+        query
+        |> where([f], f.pack_id == ^pack_id)
+        |> Repo.all()
+    end
   end
 
   @doc """
@@ -35,7 +143,7 @@ defmodule FlashcardQuiz.Flashcards do
       ** (Ecto.NoResultsError)
 
   """
-  def get_flashcard!(id), do: Repo.get!(Flashcard, id)
+  def get_flashcard!(id), do: Repo.get!(Flashcard, id) |> Repo.preload(:pack)
 
   @doc """
   Creates a flashcard.

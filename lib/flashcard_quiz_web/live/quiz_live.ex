af904ca00eb25a54588c51f5d7a72d9998687257
Fly Dev <dev@fly.io>
2025-07-16 20:10:14 +0000

    update quiz live for pack support

--- lib/flashcard_quiz_web/live/quiz_live.ex
+++ lib/flashcard_quiz_web/live/quiz_live.ex
@@ -3,125 +3,130 @@ defmodule FlashcardQuizWeb.QuizLive do
 
   alias FlashcardQuiz.Flashcards
 
-  @topic "flashcard_quiz"
+  @impl true
+  def mount(params, _session, socket) do
+    pack_id = params["pack_id"]
 
-  def mount(_params, _session, socket) do
-    if connected?(socket) do
-      Phoenix.PubSub.subscribe(FlashcardQuiz.PubSub, @topic)
+    flashcards =
+      if pack_id do
+        Flashcards.list_flashcards_by_pack(String.to_integer(pack_id))
+      else
+        Flashcards.list_flashcards()
+      end
+
+    if flashcards == [] do
+      {:ok,
+       socket
+       |> put_flash(:info, "No flashcards available. Create some flashcards first!")
+       |> push_navigate(to: "/manage")}
+    else
+      {:ok,
+       socket
+       |> assign(:pack_id, pack_id)
+       |> assign(:flashcards, flashcards)
+       |> assign(:current_index, 0)
+       |> assign(:show_answer, false)
+       |> assign(:score, 0)
+       |> assign(:answered_count, 0)
+       |> assign(:current_card, hd(flashcards))}
     end
-
-    flashcards = Flashcards.list_flashcards()
-
-    {:ok,
-     socket
-     |> assign(:flashcards, flashcards)
-     |> assign(:current_index, 0)
-     |> assign(:score, 0)
-     |> assign(:total_answered, 0)
-     |> assign(:showing_answer, false)
-     |> assign(:quiz_complete, false)
-     |> assign(:flashcards_empty?, flashcards == [])}
   end
 
+  @impl true
   def handle_event("flip_card", _params, socket) do
-    {:noreply, assign(socket, :showing_answer, !socket.assigns.showing_answer)}
+    {:noreply, assign(socket, :show_answer, !socket.assigns.show_answer)}
   end
 
+  @impl true
   def handle_event("mark_correct", _params, socket) do
     new_score = socket.assigns.score + 1
-    new_total = socket.assigns.total_answered + 1
+    new_answered_count = socket.assigns.answered_count + 1
 
-    # Broadcast score update
-    Phoenix.PubSub.broadcast(FlashcardQuiz.PubSub, @topic, {:score_update, new_score, new_total})
+    socket =
+      socket
+      |> assign(:score, new_score)
+      |> assign(:answered_count, new_answered_count)
+      |> next_card()
 
-    {:noreply,
-     socket
-     |> assign(:score, new_score)
-     |> assign(:total_answered, new_total)
-     |> assign(:showing_answer, false)
-     |> next_card()}
+    {:noreply, socket}
   end
 
+  @impl true
   def handle_event("mark_incorrect", _params, socket) do
-    new_total = socket.assigns.total_answered + 1
+    new_answered_count = socket.assigns.answered_count + 1
 
-    # Broadcast score update
-    Phoenix.PubSub.broadcast(
-      FlashcardQuiz.PubSub,
-      @topic,
-      {:score_update, socket.assigns.score, new_total}
-    )
+    socket =
+      socket
+      |> assign(:answered_count, new_answered_count)
+      |> next_card()
 
-    {:noreply,
-     socket
-     |> assign(:total_answered, new_total)
-     |> assign(:showing_answer, false)
-     |> next_card()}
+    {:noreply, socket}
   end
 
+  @impl true
   def handle_event("next_card", _params, socket) do
     {:noreply, next_card(socket)}
   end
 
+  @impl true
   def handle_event("previous_card", _params, socket) do
     {:noreply, previous_card(socket)}
   end
 
+  @impl true
   def handle_event("reset_quiz", _params, socket) do
     {:noreply,
      socket
      |> assign(:current_index, 0)
+     |> assign(:show_answer, false)
      |> assign(:score, 0)
-     |> assign(:total_answered, 0)
-     |> assign(:showing_answer, false)
-     |> assign(:quiz_complete, false)}
-  end
-
-  def handle_info({:score_update, score, total}, socket) do
-    {:noreply,
-     socket
-     |> assign(:score, score)
-     |> assign(:total_answered, total)}
+     |> assign(:answered_count, 0)
+     |> assign(:current_card, hd(socket.assigns.flashcards))}
   end
 
   defp next_card(socket) do
     flashcards = socket.assigns.flashcards
     current_index = socket.assigns.current_index
+    new_index = min(current_index + 1, length(flashcards) - 1)
 
-    if current_index + 1 >= length(flashcards) do
-      assign(socket, :quiz_complete, true)
-    else
-      assign(socket, :current_index, current_index + 1)
-    end
+    socket
+    |> assign(:current_index, new_index)
+    |> assign(:show_answer, false)
+    |> assign(:current_card, Enum.at(flashcards, new_index))
   end
 
   defp previous_card(socket) do
+    flashcards = socket.assigns.flashcards
     current_index = socket.assigns.current_index
+    new_index = max(current_index - 1, 0)
 
-    if current_index > 0 do
-      assign(socket, :current_index, current_index - 1)
-    else
-      socket
-    end
+    socket
+    |> assign(:current_index, new_index)
+    |> assign(:show_answer, false)
+    |> assign(:current_card, Enum.at(flashcards, new_index))
   end
 
-  defp current_card(flashcards, index) do
-    Enum.at(flashcards, index)
+  defp progress_percentage(current_index, total_cards) do
+    if total_cards > 0 do
+      round((current_index + 1) / total_cards * 100)
+    else
+      0
+    end
   end
 
   defp markdown_to_html(text) do
     text
     |> String.replace(
       ~r/```elixir\n(.*?)\n```/s,
-      "<pre class=\"bg-slate-100 p-4 rounded-lg overflow-x-auto\"><code class=\"language-elixir text-sm\">\\1</code></pre>"
+      "<pre class=\"bg-slate-100 p-4 rounded-lg text-sm font-mono overflow-x-auto\"><code class=\"language-elixir\">\\1</code></pre>"
     )
     |> String.replace(
       ~r/```(\w+)?\n(.*?)\n```/s,
-      "<pre class=\"bg-slate-100 p-4 rounded-lg overflow-x-auto\"><code class=\"text-sm\">\\2</code></pre>"
+      "<pre class=\"bg-slate-100 p-4 rounded-lg text-sm font-mono overflow-x-auto\"><code>\\2</code></pre>"
     )
     |> String.replace(
       ~r/`([^`]+)`/,
-      "<code class=\"bg-slate-100 px-1 py-0.5 rounded text-sm\">\\1</code>"
+      "<code class=\"bg-slate-100 px-2 py-1 rounded text-sm font-mono\">\\1</code>"
     )
     |> String.replace(~r/\*\*(.*?)\*\*/, "<strong>\\1</strong>")
     |> String.replace(~r/\*(.*?)\*/, "<em>\\1</em>")

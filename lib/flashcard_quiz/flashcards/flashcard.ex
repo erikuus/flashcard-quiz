3a40080f86e09e3bda7c1c35ada366a94a130caa
Fly Dev <dev@fly.io>
2025-07-16 19:52:15 +0000

    add pack_id to flashcard schema

--- lib/flashcard_quiz/flashcards/flashcard.ex
+++ lib/flashcard_quiz/flashcards/flashcard.ex
@@ -2,9 +2,13 @@ defmodule FlashcardQuiz.Flashcards.Flashcard do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias FlashcardQuiz.Flashcards.Pack
+
   schema "flashcards" do
     field :front, :string
     field :back, :string
+
+    belongs_to :pack, Pack
 
     timestamps()
   end
@@ -12,9 +16,10 @@ defmodule FlashcardQuiz.Flashcards.Flashcard do
   @doc false
   def changeset(flashcard, attrs) do
     flashcard
-    |> cast(attrs, [:front, :back])
+    |> cast(attrs, [:front, :back, :pack_id])
     |> validate_required([:front, :back])
-    |> validate_length(:front, min: 1, max: 1000)
-    |> validate_length(:back, min: 1, max: 1000)
+    |> validate_length(:front, min: 1, max: 500)
+    |> validate_length(:back, min: 1, max: 2000)
+    |> foreign_key_constraint(:pack_id)
   end
 end

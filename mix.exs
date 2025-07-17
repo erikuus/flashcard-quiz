cb4db4c21cb1b518fc5dc9f94f217728c297c986
Fly Dev <dev@fly.io>
2025-07-17 18:08:42 +0000

    add earmark dependency

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule FlashcardQuiz.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"}
-    ]
+      {:earmark, "~> 1.4"},
+]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
